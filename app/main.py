997b5518a1847e18d155f2dc59aab05ce2a69f42
rpr110 <117991038+rpr110@users.noreply.github.com>
2024-03-30 10:34:06 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI 
+import socket
+import random
+import time
+import subprocess
+from fastapi import FastAPI
 
 app = FastAPI(
-    title="FastAPI - Hello World",
-    description="This is the Hello World of FastAPI.",
+    title="FastAPI - System and AWS Info",
+    description="Returns system and AWS-related information along with a random number.",
     version="1.0.0",
-) 
+)
+
 
 
 @app.get("/")
-def hello_world():
-    return {"Hello": "World"}
+def get_info():
+    user_ip = socket.gethostbyname(socket.gethostname())
+    system_ip = socket.gethostbyname(socket.getfqdn())
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    random_number = random.randint(1, 1000)
+    return {
+        "User IP": user_ip,
+        "System IP": system_ip,
+        "AWS Region": None,
+        "AWS Availability Zone": None,
+        "Current Time": current_time,
+        "Random Number": random_number
+    }
